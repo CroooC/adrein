5e80cfb11e0052fd3b3bba84cfd808c046c5a728
CrooC <axelderoque@gmail.com>
2024-10-08 15:29:26 +0200

    Adding back arrow to games, apps, and functionalities

--- ADR/games/views.py
+++ ADR/games/views.py
@@ -4,27 +4,27 @@ from django.urls import reverse
 # Mini Games
 
 def blackjack(request):
-    return render(request, 'games/blackjack.html')
+    return render(request, 'games/blackjack.html', {'page_type': 'game'})
 
 def hangman(request):
-    return render(request, 'games/hangman.html')
+    return render(request, 'games/hangman.html', {'page_type': 'game'})
 
 def dodger(request):
-    return render(request, 'games/dodger.html')
+    return render(request, 'games/dodger.html', {'page_type': 'game'})
 
 def worm(request):
-    return render(request, 'games/worm.html')
+    return render(request, 'games/worm.html', {'page_type': 'game'})
 
 def tetris(request):
-    return render(request, 'games/tetris.html')
+    return render(request, 'games/tetris.html', {'page_type': 'game'})
 
 def bonneteau(request):
-    return render(request, 'games/bonneteau.html')
+    return render(request, 'games/bonneteau.html', {'page_type': 'game'})
 
 # MultiPlayer Games
 
 def tictactoe(request):
-    return render(request, 'games/tictactoe.html')
+    return render(request, 'games/tictactoe.html', {'page_type': 'multi_game'})
 
 def pong(request):
-    return render(request, 'games/pong.html')
+    return render(request, 'games/pong.html', {'page_type': 'multi_game'})
